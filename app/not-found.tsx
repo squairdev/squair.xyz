1b1b09944362bb12a0baf0f20cee2b64102214fc
squairdev <me@squair.xyz>
2026-04-19 16:11:14 -0400

    Update 404

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -9,39 +9,26 @@ import { ReactLenis} from 'lenis/react'
 export default function Home() {
   return (
     <section className="font relative h-[100vh] sm:overflow-hidden">
-      
-      <div style={{ width: '100%', height: '100%', position: 'fixed', overflow: 'auto', zIndex: 0 }}>
-        <DotGrid
-          baseColor="#222222"
-          activeColor="#60a5fa"
-          proximity={120}
-          shockStrength={3}
-          resistance={1000}
-          dotSize={6}
-          gap={16}
-        />
-      </div>
       <ReactLenis root>
-      <section className='z-0 w-[100vw] h-[93vh] flex flex-cols items-center justify-center'>
-        <div className='max-w-2xl'>
+      <section className='z-10 w-[100vw] h-[93vh] flex flex-col items-center justify-center'>
+        <div className='absolute h-[100vh]'>
+          <div className='absolute w-[100rem] right-0 translate-x-1/2 -translate-y-1/2'>
+            <svg xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="0 0 800 800" opacity="0.3"><defs><filter id="bbblurry-filter-3" x="-100%" y="-100%" width="400%" height="400%" filterUnits="objectBoundingBox" primitiveUnits="userSpaceOnUse" colorInterpolationFilters="sRGB"><feGaussianBlur stdDeviation="130" x="0%" y="0%" width="100%" height="100%" in="SourceGraphic" edgeMode="none" result="blur"></feGaussianBlur></filter></defs><g filter="url(#bbblurry-filter-3)"><ellipse rx="150" ry="150" cx="400" cy="400" fill="#00a6f4"></ellipse></g></svg>
+          </div>
+          <div className='absolute w-[100rem] right-0 bottom-0 translate-x-1/2 translate-y-1/2'>
+            <svg xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="0 0 800 800" opacity="0.3"><defs><filter id="bbblurry-filter-3" x="-100%" y="-100%" width="400%" height="400%" filterUnits="objectBoundingBox" primitiveUnits="userSpaceOnUse" colorInterpolationFilters="sRGB"><feGaussianBlur stdDeviation="130" x="0%" y="0%" width="100%" height="100%" in="SourceGraphic" edgeMode="none" result="blur"></feGaussianBlur></filter></defs><g filter="url(#bbblurry-filter-3)"><ellipse rx="150" ry="150" cx="400" cy="400" fill="#00a6f4"></ellipse></g></svg>
+          </div>
+        </div>
+        <div className='max-w-2xl flex flex-col items-center justify-center'>
           <div className='text-5xl md:text-7xl font-bold mb-4 leading-20 mx-auto text-center'>
             <BlurText text="404" direction="bottom" delay={70} className="justify-center" />
           </div>
           <div className='mx-auto text-center pt-5 text-[1.5rem] max-w-[80vw] lg:max-w-[30vw] text-[#ababab] pb-10'>
-            <AnimatedContent direction="horizontal" distance={50} delay={0.3} scale={0.7}>
-              <svg className='absolute' style={{transform: "translate(-100%, -100%)"}} xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="M460-160v-300H160v-40h300v-300h40v300h300v40H500v300h-40Z"/></svg>
-            </AnimatedContent>
             <FadeContent blur delay={0.6} duration={0.76}>{"Either this page doesn't exist, or I removed it in a newer version of this website."}</FadeContent>
             <div style={{flexGrow:"1"}}/>
-            <AnimatedContent reverse direction="horizontal" distance={50} delay={0.3} scale={0.7}>
-              <svg style={{transform: "translate(50%, -0%)", marginLeft: 'auto'}} xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="M460-160v-300H160v-40h300v-300h40v300h300v40H500v300h-40Z"/></svg>
-            </AnimatedContent>
           </div>
-          <div className='gap-2 flex justify-center align-center flex-cols max-w-[80vw] mx-auto'>
-            <AnimatedContent direction="vertical" duration={0.2} distance={50} delay={0.5} scale={0.9} className='flex items-center justify-center mx-auto w-[10rem] h-[3.5rem] bg-[#1c398e]/15 border-[#1c398e]/25 border-4 rounded-2xl border-solid hover:border-[#155dfc]/25 hover:bg-[#155dfc]/15 transition-all duration-300 ease-in-out text-lg cursor-pointer'>
-              Go Home<a href='/' className='absolute w-[10rem] h-[3.5rem]'></a>
-            </AnimatedContent>
-          </div>
+          <AnimatedContent distance={10} direction='vertical' delay={0.9} className='z-[1000]'><a aria-label='Go back home' className='p-4 bg-transparent hover:bg-sky-500 text-white hover:text-black border-sky-500 border-2 text-xl transition-all duration-200 ease-in-out' href='/'>Go Home</a></AnimatedContent>
+
         </div>
       </section>
       <div className='relative flex align-center justify-center -top-5'>
